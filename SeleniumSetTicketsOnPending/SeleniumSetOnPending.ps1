d9a79a22254da8187d3c25a31936985b7085aebf
Jakub-Syrek <40210613+Jakub-Syrek@users.noreply.github.com>
2019-01-22 01:04:37 +0100

    Incidents automanaged, work orders to go.

--- SeleniumSetTicketsOnPending/SeleniumSetOnPending.ps1
+++ SeleniumSetTicketsOnPending/SeleniumSetOnPending.ps1
@@ -131,9 +131,9 @@ $textBox3.width = 200;
 
 #############Define default values for the input boxes
 
-$defaultValue1 = “jakub.syrek@hcl.com”
+$defaultValue1 = “”
 $defaultValue2 = “”
-$defaultValue3 = “GEO”
+$defaultValue3 = “”
 
 $textBox1.Text = $defaultValue1;
 
@@ -204,104 +204,6 @@ return $textBox1.Text, $textBox2.Text, $textBox3.Text
  
 
 }
-
-
-<#
-function Read-MultiLineInputBoxDialog([string]$Message, [string]$WindowTitle, [string]$DefaultText)
-    {
-        Add-Type -AssemblyName System.Drawing
-        Add-Type -AssemblyName System.Windows.Forms
-     
-        # Create the Label.
-        $label = New-Object System.Windows.Forms.Label
-        $label.Location = New-Object System.Drawing.Size(10,10) 
-        $label.Size = New-Object System.Drawing.Size(280,20)
-        $label.AutoSize = $true
-        $label.Text = $Message
-     
-        # Create the TextBox used to capture the user's text.
-        $textBox = New-Object System.Windows.Forms.TextBox 
-        $textBox.Location = New-Object System.Drawing.Size(10,40) 
-        $textBox.Size = New-Object System.Drawing.Size(575,200)
-        $textBox.AcceptsReturn = $true
-        $textBox.AcceptsTab = $false
-        $textBox.Multiline = $true
-        $textBox.ScrollBars = 'Both'
-        $textBox.Text = $DefaultText
-     
-        # Create the OK button.
-        $okButton = New-Object System.Windows.Forms.Button
-        $okButton.Location = New-Object System.Drawing.Size(415,250)
-        $okButton.Size = New-Object System.Drawing.Size(75,25)
-        $okButton.Text = "OK"
-        $okButton.Add_Click({ [array]$arr = $textBox.Text; $form.Tag = $textBox.Text; $form.Close() })  #$form.Tag
-     
-        # Create the Cancel button.
-        $cancelButton = New-Object System.Windows.Forms.Button
-        $cancelButton.Location = New-Object System.Drawing.Size(510,250)
-        $cancelButton.Size = New-Object System.Drawing.Size(75,25)
-        $cancelButton.Text = "Cancel"
-        $cancelButton.Add_Click({ [arr]$arr = $null;$form.Tag = $null ; $form.Close() })
-     
-        # Create the form.
-        $form = New-Object System.Windows.Forms.Form 
-        $form.Text = $WindowTitle
-        $form.Size = New-Object System.Drawing.Size(610,320)
-        $form.FormBorderStyle = 'FixedSingle'
-        $form.StartPosition = "CenterScreen"
-        $form.AutoSizeMode = 'GrowAndShrink'
-        $form.Topmost = $True
-        $form.AcceptButton = $okButton
-        $form.CancelButton = $cancelButton
-        $form.ShowInTaskbar = $true
-     
-        # Add all of the controls to the form.
-        $form.Controls.Add($label)
-        $form.Controls.Add($textBox)
-        $form.Controls.Add($okButton)
-        $form.Controls.Add($cancelButton)
-     
-        # Initialize and show the form.
-        $form.Add_Shown({$form.Activate()})
-        $form.ShowDialog() > $null   # Trash the text of the button that was clicked.
-     
-        # Return the text that the user entered.
-        [string]$str = $form.Tag
-        return $str
-
-    }
-
-     
-  
-    
-
-    [string]$string  = "";
-    $string = Read-MultiLineInputBoxDialog -Message "Please Provide list of hosts to be deleted" -WindowTitle "List of hosts" -DefaultText "Enter some text here..." ;
-    
-    $array = $string.split([Environment]::NewLine)
-    
- 
-    
-
-    if ($strinG -eq $null) { Write-Host "You clicked Cancel" }
-    else { Write-Host "You entered the following text: $strinG" }  
-      
-     [array]$Result = @() ;
-       
-        foreach ($a in $array)
-     {
-      if ($a -ne "") {  $Result += $a ; }
-      
-     }
-       
-     if ($Result -eq $null) { Write-Host "No result" }
-     else { Write-Host $Result } 
-    
-
-
-    #>
-
-
 
 
 if (Get-Module -ListAvailable -Name Selenium) {
@@ -494,7 +396,7 @@ foreach ($line in $rows)
         
         Start-Sleep -Seconds 2
                 
-                #//*[@id="WIN_0_304248710"]/fieldset/div/dl/dd[3]/span[2]/a
+               
         $IThome = $Browser.FindElements([OpenQA.Selenium.By]::Xpath('//*[@id="WIN_0_304248710"]/fieldset/div/dl/dd[3]/span[2]/a'))
         $IThome[0].Click()
         Start-Sleep -Seconds 2
